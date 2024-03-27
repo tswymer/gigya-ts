543ac03819ad9b2ba9c31c63e0a4e28eb2815e92
RazvanD <razvan.daniciuc@ecenta.com>
2024-03-27 13:50:45 +0200

    Omit password param is register response for webSDK

--- packages/web-sdk/src/namespaces/accounts.ts
+++ packages/web-sdk/src/namespaces/accounts.ts
@@ -172,7 +172,7 @@ export type AccountsRegisterResponseJS<
     DataSchema extends GigyaData,
     PreferencesSchema extends GigyaPreferences,
     SubscriptionsSchema extends GigyaSubscriptions,
-> = AccountsRegisterResponse<DataSchema, PreferencesSchema, SubscriptionsSchema>;
+> = Omit<AccountsRegisterResponse<DataSchema, PreferencesSchema, SubscriptionsSchema>, 'password'>;
 
 /**
  * @see https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/4134b19d70b21014bbc5a10ce4041860.html#parameters
