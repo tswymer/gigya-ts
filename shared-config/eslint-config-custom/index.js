f1ee3fb4802d61e2429c3ea5d3a8b87064bb9130
Tobias Wymer <tobias.wymer@ecenta.com>
2024-06-30 02:38:15 +0200

    Removed unused lint rule

--- shared-config/eslint-config-custom/index.js
+++ shared-config/eslint-config-custom/index.js
@@ -12,7 +12,7 @@ const config = {
         sourceType: 'module',
     },
     extends: ['eslint:recommended'],
-    plugins: ['@typescript-eslint', 'plugin:@typescript-eslint/recommended'],
+    plugins: ['@typescript-eslint'],
     ignorePatterns: ['dist/*'],
     rules: {
         'eol-last': ['error', 'always'],
