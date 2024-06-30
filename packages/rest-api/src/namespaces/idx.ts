ac49e56001d468689d358bda91291bfad1692bba
Tobias Wymer <tobias.wymer@ecenta.com>
2024-06-30 14:18:04 +0200

    Added fidm and idx namespaces to rest api

---
+++ packages/rest-api/src/namespaces/idx.ts
@@ -0,0 +1,19 @@
+import { GigyaRequest, GigyaResponse } from '../types/gigya-helpers';
+
+/**
+ * The method deletes a Dataflow by ID and deletes also all the drafts related to the dataflow.
+ *  
+ * @see https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/415e96a670b21014bbc5a10ce4041860.html#parameters
+ */
+export type IDXDeleteDataflowRequest = GigyaRequest<{
+
+}>;
+
+/**
+ * @see https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/415e96a670b21014bbc5a10ce4041860.html#response-data
+ */
+export type IDXDeleteDataflowResponse = GigyaResponse<{}>;
+
+export type GigyaIDXNamespace = {
+    deleteDataflow: (params: IDXDeleteDataflowRequest) => IDXDeleteDataflowResponse;
+};
