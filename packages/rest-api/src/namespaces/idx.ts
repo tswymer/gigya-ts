08a6a9d971c1bbebbffb9e5f5566849882399419
Tobias Wymer <tobias.wymer@ecenta.com>
2024-08-03 20:02:52 +0200

    Cleaned up comments

--- packages/rest-api/src/namespaces/idx.ts
+++ packages/rest-api/src/namespaces/idx.ts
@@ -1,8 +1,6 @@
 import { GigyaRequest, GigyaResponse } from '../types/gigya-requests';
 
 /**
- * The method deletes a Dataflow by ID and deletes also all the drafts related to the dataflow.
- *
  * @see https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/415e96a670b21014bbc5a10ce4041860.html#parameters
  */
 export type IDXDeleteDataflowRequest = GigyaRequest<{}>;
@@ -13,5 +11,10 @@ export type IDXDeleteDataflowRequest = GigyaRequest<{}>;
 export type IDXDeleteDataflowResponse = GigyaResponse<{}>;
 
 export type GigyaIDXNamespace = {
+    /**
+     * The method deletes a Dataflow by ID and deletes also all the drafts related to the dataflow.
+     * 
+     * @see https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/415e96a670b21014bbc5a10ce4041860.html
+     */
     deleteDataflow: (params: IDXDeleteDataflowRequest) => Promise<IDXDeleteDataflowResponse>;
 };
