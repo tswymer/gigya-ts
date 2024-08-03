d4f20da52b4c6f115ec25d7f55669ae011ec56f3
Tobias Wymer <tobias.wymer@ecenta.com>
2024-08-03 21:03:31 +0200

    Cleaned up audit comments

--- packages/rest-api/src/namespaces/audit.ts
+++ packages/rest-api/src/namespaces/audit.ts
@@ -2,8 +2,6 @@ import { AuditLogObject } from '../types/gigya-audit';
 import { GigyaRequest, GigyaResponse } from '../types/gigya-requests';
 
 /**
- * The method enables you to search your site's audit log using an SQL-like query. A short delay is possible between the writing of audit log data and its availability in queries.
- *
  * @see https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/4143815a70b21014bbc5a10ce4041860.html
  */
 export type AuditSearchRequest = GigyaRequest<{
@@ -46,5 +44,10 @@ export type AuditSearchResponse = GigyaResponse<{
 }>;
 
 export type GigyaAuditNamespace = {
+    /**
+     * The method enables you to search your site's audit log using an SQL-like query. A short delay is possible between the writing of audit log data and its availability in queries.
+     * 
+     * @see https://help.sap.com/docs/SAP_CUSTOMER_DATA_CLOUD/8b8d6fffe113457094a17701f63e3d6a/4143815a70b21014bbc5a10ce4041860.html
+     */
     search: (params: AuditSearchRequest) => Promise<AuditSearchResponse>;
 };
